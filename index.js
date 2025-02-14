7b60e4bafb9cc3685016384115bcbf92ec184640
romkas228 <romandanuliyk@gmail.com>
2025-02-14 19:07:44 +0200

    upd

---
+++ index.js
@@ -0,0 +1,530 @@
+const images =[
+    {
+        id: 1,
+        link: './images/1.jpg'
+    },
+    {
+        id: 2,
+        link: './images/2.jpg'
+    },
+    {
+        id: 3,
+        link: './images/3.jpg'
+    },
+    {
+        id: 4,
+        link: './images/4.jpg'
+    },
+    {
+        id: 5,
+        link: './images/5.jpg'
+    },
+    {
+        id: 6,
+        link: './images/6.jpg'
+    },
+    {
+        id: 7,
+        link: './images/7.jpg'
+    },
+    {
+        id: 8,
+        link: './images/8.jpg'
+    },
+    {
+        id: 9,
+        link: './images/9.jpg'
+    },
+    {
+        id: 10,
+        link: './images/10.jpg'
+    },
+    {
+        id: 11,
+        link: './images/11.jpg'
+    },
+    {
+        id: 12,
+        link: './images/11.jpg'
+    },
+    {
+        id: 13,
+        link: './images/12.jpg'
+    },
+    {
+        id: 14,
+        link: './images/13.jpg'
+    },
+    {
+        id: 15,
+        link: './images/14.jpg'
+    },
+    {
+        id: 16,
+        link: './images/15.jpg'
+    },
+    {
+        id: 17,
+        link: './images/16.jpg'
+    },
+    {
+        id: 18,
+        link: './images/17.jpg'
+    },
+    {
+        id: 19,
+        link: './images/18.jpg'
+    },
+    {
+        id: 20,
+        link: './images/19.jpg'
+    },
+    {
+        id: 21,
+        link: './images/20.jpg'
+    },
+    {
+        id: 22,
+        link: './images/21.jpg'
+    },
+    {
+        id: 23,
+        link: './images/22.jpg'
+    },
+    {
+        id: 24,
+        link: './images/23.jpg'
+    },
+    {
+        id: 25,
+        link: './images/24.jpg'
+    },
+    {
+        id: 26,
+        link: './images/25.jpg'
+    },
+    {
+        id: 27,
+        link: './images/26.jpg'
+    },
+    {
+        id: 28,
+        link: './images/28.jpg'
+    },
+    {
+        id: 29,
+        link: './images/29.jpg'
+    },
+    {
+        id: 30,
+        link: './images/30.jpg'
+    },
+    {
+        id: 31,
+        link: './images/31.jpg'
+    },
+    {
+        id: 32,
+        link: './images/32.jpg'
+    },
+    {
+        id: 33,
+        link: './images/33.jpg'
+    },
+    {
+        id: 34,
+        link: './images/34.jpg'
+    },
+    {
+        id: 35,
+        link: './images/35.jpg'
+    },
+    {
+        id: 36,
+        link: './images/36.jpg'
+    },
+    {
+        id: 37,
+        link: './images/37.jpg'
+    },
+    {
+        id: 38,
+        link: './images/38.jpg'
+    },
+    {
+        id: 39,
+        link: './images/39.jpg'
+    },
+    {
+        id: 40,
+        link: './images/40.jpg'
+    },
+    {
+        id: 41,
+        link: './images/41.jpg'
+    },
+    {
+        id: 42,
+        link: './images/42.jpg'
+    },
+    {
+        id: 43,
+        link: './images/43.jpg'
+    },
+    {
+        id: 44,
+        link: './images/44.jpg'
+    },
+    {
+        id: 45,
+        link: './images/45.jpg'
+    },
+    {
+        id: 46,
+        link: './images/46.jpg'
+    },
+    {
+        id: 47,
+        link: './images/47.jpg'
+    },
+    {
+        id: 48,
+        link: './images/48.jpg'
+    },
+    {
+        id: 49,
+        link: './images/49.jpg'
+    },
+    {
+        id: 50,
+        link: './images/50.jpg'
+    },
+    {
+        id: 51,
+        link: './images/51.jpg'
+    },
+    {
+        id: 52,
+        link: './images/52.jpg'
+    },
+    {
+        id: 53,
+        link: './images/53.jpg'
+    },
+    {
+        id: 54,
+        link: './images/54.jpg'
+    },
+    {
+        id: 55,
+        link: './images/55.jpg'
+    },
+    {
+        id: 56,
+        link: './images/56.jpg'
+    },
+    {
+        id: 57,
+        link: './images/57.jpg'
+    },
+    {
+        id: 58,
+        link: './images/58.jpg'
+    },
+    {
+        id: 59,
+        link: './images/59.jpg'
+    },
+    {
+        id: 60,
+        link: './images/60.jpg'
+    },
+    {
+        id: 61,
+        link: './images/61.jpg'
+    },
+    {
+        id: 62,
+        link: './images/62.jpg'
+    },
+    {
+        id: 63,
+        link: './images/63.jpg'
+    },
+    {
+        id: 64,
+        link: './images/64.jpg'
+    },
+    {
+        id: 65,
+        link: './images/65.jpg'
+    },
+    {
+        id: 66,
+        link: './images/66.jpg'
+    },
+    {
+        id: 67,
+        link: './images/67.jpg'
+    },
+    {
+        id: 68,
+        link: './images/68.jpg'
+    },
+    {
+        id: 69,
+        link: './images/69.jpg'
+    },
+    {
+        id: 70,
+        link: './images/70.jpg'
+    },
+    {
+        id: 71,
+        link: './images/71.jpg'
+    },
+    {
+        id: 72,
+        link: './images/72.jpg'
+    },
+    {
+        id: 73,
+        link: './images/73.jpg'
+    },
+    {
+        id: 74,
+        link: './images/74.jpg'
+    },
+    {
+        id: 75,
+        link: './images/75.jpg'
+    },
+    {
+        id: 76,
+        link: './images/76.jpg'
+    },
+    {
+        id: 77,
+        link: './images/77.jpg'
+    },
+    {
+        id: 78,
+        link: './images/78.jpg'
+    },
+    {
+        id: 79,
+        link: './images/79.jpg'
+    },
+    {
+        id: 80,
+        link: './images/80.jpg'
+    },
+    {
+        id: 81,
+        link: './images/81.jpg'
+    },
+    {
+        id: 82,
+        link: './images/82.jpg'
+    },
+    {
+        id: 83,
+        link: './images/83.jpg'
+    },
+    {
+        id: 84,
+        link: './images/84.jpg'
+    },
+    {
+        id: 85,
+        link: './images/85.jpg'
+    },
+    {
+        id: 86,
+        link: './images/86.jpg'
+    },
+    {
+        id: 87,
+        link: './images/87.jpg'
+    },
+    {
+        id: 88,
+        link: './images/88.jpg'
+    },
+    {
+        id: 89,
+        link: './images/89.jpg'
+    },
+    {
+        id: 90,
+        link: './images/80.jpg'
+    },
+    {
+        id: 91,
+        link: './images/91.jpg'
+    },
+    {
+        id: 92,
+        link: './images/92.jpg'
+    },
+    {
+        id: 93,
+        link: './images/93.jpg'
+    },
+    {
+        id: 94,
+        link: './images/94.jpg'
+    },
+    {
+        id: 95,
+        link: './images/95.jpg'
+    },
+    {
+        id: 96,
+        link: './images/96.jpg'
+    },
+    {
+        id: 97,
+        link: './images/97.jpg'
+    },
+    {
+        id: 98,
+        link: './images/98.jpg'
+    },
+    {
+        id: 99,
+        link: './images/99.jpg'
+    },
+    {
+        id: 100,
+        link: './images/100.jpg'
+    },
+    {
+        id: 101,
+        link: './images/101.jpg'
+    },
+    {
+        id: 102,
+        link: './images/102.jpg'
+    },
+    {
+        id: 103,
+        link: './images/103.jpg'
+    },
+    {
+        id: 104,
+        link: './images/104.jpg'
+    },
+    {
+        id: 105,
+        link: './images/105.jpg'
+    },
+    {
+        id: 106,
+        link: './images/106.jpg'
+    },
+    {
+        id: 107,
+        link: './images/107.jpg'
+    },
+    {
+        id: 108,
+        link: './images/108.jpg'
+    },
+    {
+        id: 109,
+        link: './images/109.jpg'
+    },
+    {
+        id: 110,
+        link: './images/110.jpg'
+    },
+    {
+        id: 111,
+        link: './images/111.jpg'
+    },
+    {
+        id: 112,
+        link: './images/112.jpg'
+    },
+    {
+        id: 113,
+        link: './images/113.jpg'
+    },
+    {
+        id: 114,
+        link: './images/114.jpg'
+    },
+    {
+        id: 115,
+        link: './images/115.jpg'
+    },
+    {
+        id: 116,
+        link: './images/116.jpg'
+    },
+    {
+        id: 117,
+        link: './images/117.jpg'
+    },
+    {
+        id: 118,
+        link: './images/118.jpg'
+    },
+    {
+        id: 119,
+        link: './images/119.jpg'
+    },
+    {
+        id: 120,
+        link: './images/120.jpg'
+    },
+    {
+        id: 121,
+        link: './images/121.jpg'
+    },
+    {
+        id: 122,
+        link: './images/122.jpg'
+    },
+    {
+        id: 123,
+        link: './images/123.jpg'
+    },
+    {
+        id: 124,
+        link: './images/124.jpg'
+    },
+    {
+        id: 125,
+        link: './images/125.jpg'
+    },
+    {
+        id: 126,
+        link: './images/126.jpg'
+    },
+    
+
+]
+const gallery = document.querySelector(".gallery")
+
+const createImg = (img , id) =>{
+    return `<img src="${img}" alt="${id}" data-original="${img}" class="images">`
+}
+
+const galleryHTML = images.map(img => createImg(img.link,img.id)).join('')
+
+gallery.innerHTML = galleryHTML
+
+gallery.addEventListener('click',(event)=>{
+    let target = event.target
+    if (target.tagName != "IMG") return
+    handleClickImg(target)
+})
+
+const handleClickImg = (img) => {
+    console.log(img)
+    basicLightbox.create(
+        `<img src="${img.dataset.original}"  width="800" height="600">`
+    ).show() 
+}
